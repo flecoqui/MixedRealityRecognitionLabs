20232336cb123cbb91e3075667fa851c0fa090da
Fred Le Coquil <flecoqui@hotmail.com>
2018-03-13 10:51:21 +0100

    add partial 6

---
+++ Lab6/VF_DynamicReco/Assets/Scripts/UIItems.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using AssetBundles;
+
+public class UIItems : MonoBehaviour {
+    private Dropdown objectList;
+    private List<string> LocalItemsKey = new List<string>();
+    private Dictionary<string, localDB.ObjectModel> localItemsDictionary = new Dictionary<string, localDB.ObjectModel>();
+    private void Awake()
+    {
+        this.objectList = GetComponentInChildren<Dropdown>();
+        if (this.objectList == null)
+        {
+            Debug.LogError("There wasn't a DropDown in the GameObject hierarchy.\n");
+            return;
+        }
+        // populate the list
+        this.objectList.ClearOptions();
+        List<localDB.ObjectModel> list =  localDB.GetObjectList();
+        foreach (var o in list)
+        {
+            localItemsDictionary.Add(o.ID, o);
+            LocalItemsKey.Add(o.ID);
+        }
+
+        this.objectList.AddOptions(this.LocalItemsKey);
+
+
+        var buttonList = GetComponentsInChildren<Button>();
+        if (buttonList.Length == 0)
+        {
+            Debug.LogError("There are no buttons in the GameObject hierarchy.\n");
+            Debug.LogError("Place this script on the root of the UI.");
+
+            return;
+        }
+
+        foreach (var button in buttonList)
+        {
+            var label = button.GetComponentInChildren<Text>();
+            button.onClick.AddListener(() => Button_OnClick(label));
+        }
+    }
+    bool GetPrimitiveType(string name, out PrimitiveType type)
+    {
+        bool result = true;
+        type = PrimitiveType.Capsule;
+        switch (name)
+        {
+            case "Sphere":
+                type = PrimitiveType.Sphere;
+                break;
+            case "Capsule":
+                type = PrimitiveType.Capsule;
+                break;
+            case "Cylinder":
+                type = PrimitiveType.Cylinder;
+                break;
+            case "Cube":
+                type = PrimitiveType.Cube;
+                break;
+            case "Plane":
+                type = PrimitiveType.Plane;
+                break;
+            case "Quad":
+                type = PrimitiveType.Quad;
+                break;
+            default:
+                result = false;
+                break;
+        }
+        return result;
+    }
+    private void Button_OnClick(Text label)
+    {
+        if (label == null)
+        {
+            return;
+        }
+
+        switch (label.text)
+        {
+            case "Open":
+                var selectedItem = objectList.options[objectList.value].text;
+                var Item = localItemsDictionary[selectedItem];
+                if (Item != null)
+                {
+                    if (Item.Type == localDB.ObjectModel.TypePrimitive)
+                    {
+                        PrimitiveType pt = PrimitiveType.Capsule;
+
+                        if (GetPrimitiveType(Item.Name, out pt) == true)
+                        {
+                            GameObject obj = GameObject.CreatePrimitive(pt);
+                            obj.transform.localPosition = new Vector3(Item.positionX, Item.positionY, Item.positionZ);
+                            obj.transform.eulerAngles = new Vector3(Item.rotationX, Item.rotationY, Item.rotationZ);
+                            obj.transform.localScale = new Vector3(Item.scaleX, Item.scaleY, Item.scaleZ);
+                            obj.transform.parent = gameObject.transform;
+                        }
+                    }
+                    else if (Item.Type == localDB.ObjectModel.TypeLocalPrefab)
+                    {
+                        Object res = Resources.Load(Item.Name, typeof(GameObject));
+                        if (res != null) {
+                            GameObject obj = Instantiate(res) as GameObject;
+                            obj.transform.localPosition = new Vector3(Item.positionX, Item.positionY, Item.positionZ);
+                            obj.transform.eulerAngles = new Vector3(Item.rotationX, Item.rotationY, Item.rotationZ);
+                            obj.transform.localScale = new Vector3(Item.scaleX, Item.scaleY, Item.scaleZ);
+                            obj.transform.parent = gameObject.transform;
+                        }
+                    }
+                    else if (Item.Type == localDB.ObjectModel.TypeLocalAssetBundle)
+                    {
+                        var myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, Item.AssetBundleName));
+                        if (myLoadedAssetBundle == null)
+                        {
+                            Debug.Log("Failed to load AssetBundle from " + Application.streamingAssetsPath + "!");
+                            return;
+                        }
+
+                        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>(Item.Name);
+                        if (prefab != null)
+                        {
+
+                            GameObject obj = Instantiate(prefab) as GameObject;
+                            obj.transform.localPosition = new Vector3(Item.positionX, Item.positionY, Item.positionZ);
+                            obj.transform.eulerAngles = new Vector3(Item.rotationX, Item.rotationY, Item.rotationZ);
+                            obj.transform.localScale = new Vector3(Item.scaleX, Item.scaleY, Item.scaleZ);
+                            obj.transform.parent = gameObject.transform;
+                        }
+                    }
+                    else if (Item.Type == localDB.ObjectModel.TypeRemoteAssetBundle)
+                    {
+                        // This is simply to get the elapsed time for this phase of AssetLoading.
+                        float startTime = Time.realtimeSinceStartup;
+                        AssetBundleManager.SetSourceAssetBundleURL(Item.AssetBundleUri);
+                        var request = AssetBundleManager.Initialize();
+                        if (request != null)
+                        {
+                            var req = AssetBundleManager.LoadAssetAsync(Item.AssetBundleName, Item.Name, typeof(GameObject));
+                            if (req != null)
+                            {
+
+                                // Get the asset.
+                                GameObject prefab = request.GetAsset<GameObject>();
+                                // code below could be used to position the AssetBundles
+                                //prefab.transform.position = new Vector3(0,1,4); 
+                                if (prefab != null)
+                                {
+
+                                    GameObject obj = Instantiate(prefab) as GameObject;
+                                    obj.transform.localPosition = new Vector3(Item.positionX, Item.positionY, Item.positionZ);
+                                    obj.transform.eulerAngles = new Vector3(Item.rotationX, Item.rotationY, Item.rotationZ);
+                                    obj.transform.localScale = new Vector3(Item.scaleX, Item.scaleY, Item.scaleZ);
+                                    obj.transform.parent = gameObject.transform;
+                                }
+
+                                // Calculate and display the elapsed time.
+                                float elapsedTime = Time.realtimeSinceStartup - startTime;
+                                Debug.Log(Item.Name + (prefab == null ? " was not" : " was") + " loaded successfully in " + elapsedTime + " seconds");
+                            }
+                        }
+                    }
+                }
+                break;
+
+            default:
+                Debug.LogWarningFormat("Button '{0}' not handled.", label.text);
+                break;
+        }
+    }
+
+}
---
+++ Lab6/VF_DynamicReco/Assets/Scripts/localDB.cs.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  class localDB : MonoBehaviour {
+    public class ObjectModel {
+        public const string TypePrimitive = "Primitive";
+        public const string TypeLocalPrefab = "Prefab";
+        public const string TypeLocalAssetBundle = "LocalAssetBundle";
+        public const string TypeRemoteAssetBundle = "RemoteAssetBundle";
+        public string ID { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string AssetBundleName { get; set; }
+        public string AssetBundleUri { get; set; }
+        public float positionX { get; set; }
+        public float positionY { get; set; }
+        public float positionZ { get; set; }
+        public float rotationX { get; set; }
+        public float rotationY { get; set; }
+        public float rotationZ { get; set; }
+        public float scaleX { get; set; }
+        public float scaleY { get; set; }
+        public float scaleZ { get; set; }
+    }
+
+    static List<ObjectModel> objectModelItems = new List<ObjectModel> {
+    // Primitive objects
+    new ObjectModel { ID = "CubeID",  Type = ObjectModel.TypePrimitive,Name = "Cube", AssetBundleName = "", AssetBundleUri = "", positionX = -1f, positionY = 0f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 45f, scaleX = 0.2f, scaleY = 0.2f, scaleZ = 0.2f },
+    new ObjectModel  { ID = "SphereID", Type = ObjectModel.TypePrimitive, Name = "Sphere",AssetBundleName = "", AssetBundleUri = "",  positionX = 0f, positionY = 0f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 0f, scaleX = 0.2f, scaleY = 0.2f, scaleZ = 0.2f },
+    new ObjectModel { ID = "CylinderID",  Type = ObjectModel.TypePrimitive, Name = "Cylinder", AssetBundleName = "", AssetBundleUri = "", positionX = 1f, positionY = 0f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 0f, scaleX = 0.2f, scaleY = 0.2f, scaleZ = 0.2f },
+    // Prefabs objects in resources folder
+    new ObjectModel { ID = "MyCubeID",  Type = ObjectModel.TypeLocalPrefab,Name = "MyCube", AssetBundleName = "", AssetBundleUri = "", positionX = -1f, positionY = -0.3f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 45f, scaleX = 0.2f, scaleY = 0.2f, scaleZ = 0.2f },
+    new ObjectModel  { ID = "MySphereID", Type = ObjectModel.TypeLocalPrefab, Name = "MySphere", AssetBundleName = "", AssetBundleUri = "", positionX = 0f, positionY = -0.3f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 0f, scaleX = 0.2f, scaleY = 0.2f, scaleZ = 0.2f },
+    new ObjectModel { ID = "MyCylinderID",  Type = ObjectModel.TypeLocalPrefab, Name = "MyCylinder", AssetBundleName = "", AssetBundleUri = "", positionX = 1f, positionY = -0.3f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 0f, scaleX = 0.2f, scaleY = 0.2f, scaleZ = 0.2f },
+    new ObjectModel { ID = "MySkeletonID",  Type = ObjectModel.TypeLocalPrefab, Name = "MySkeleton",AssetBundleName = "", AssetBundleUri = "",  positionX = 1f, positionY = -0.3f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 0f, scaleX = 3f, scaleY = 3f, scaleZ = 3f },
+    // local AssetBundle
+    new ObjectModel { ID = "mycubeID",  Type = ObjectModel.TypeLocalAssetBundle,Name = "MyCube", AssetBundleName = "mycube",  AssetBundleUri = "",positionX = -1f, positionY = -0.6f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 45f, scaleX = 0.2f, scaleY = 0.2f, scaleZ = 0.2f },
+    new ObjectModel  { ID = "mysphereID", Type = ObjectModel.TypeLocalAssetBundle, Name = "MySphere",AssetBundleName = "mysphere",  AssetBundleUri = "", positionX = 0f, positionY = -0.6f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 0f, scaleX = 0.2f, scaleY = 0.2f, scaleZ = 0.2f },
+    new ObjectModel { ID = "mycylinderID",  Type = ObjectModel.TypeLocalAssetBundle, Name = "MyCylinder",AssetBundleName = "mycylinder",  AssetBundleUri = "", positionX = 1f, positionY = -0.6f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 0f, scaleX = 0.2f, scaleY = 0.2f, scaleZ = 0.2f },
+    new ObjectModel { ID = "myskeletonID",  Type = ObjectModel.TypeLocalAssetBundle, Name = "MySkeleton", AssetBundleName = "myskeleton", AssetBundleUri = "", positionX = 1f, positionY = -0.6f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 0f, scaleX = 3f, scaleY = 3f, scaleZ = 3f },
+    // remote AssetBundle
+    new ObjectModel { ID = "myremotecubeID",  Type = ObjectModel.TypeRemoteAssetBundle,Name = "MyCube", AssetBundleName = "mycube", AssetBundleUri = "https://mraiwebapp.azurewebsites.net/AssetBundles/", positionX = -1f, positionY = -0.9f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 45f, scaleX = 0.2f, scaleY = 0.2f, scaleZ = 0.2f },
+    new ObjectModel  { ID = "myremotesphereID", Type = ObjectModel.TypeRemoteAssetBundle, Name = "MySphere",AssetBundleName = "mysphere", AssetBundleUri = "https://mraiwebapp.azurewebsites.net/AssetBundles/",  positionX = 0f, positionY = -0.9f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 0f, scaleX = 0.2f, scaleY = 0.2f, scaleZ = 0.2f },
+    new ObjectModel { ID = "myremotecylinderID",  Type = ObjectModel.TypeRemoteAssetBundle, Name = "MyCylinder",AssetBundleName = "mycylinder", AssetBundleUri = "https://mraiwebapp.azurewebsites.net/AssetBundles/",  positionX = 1f, positionY = -0.9f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 0f, scaleX = 0.2f, scaleY = 0.2f, scaleZ = 0.2f },
+    new ObjectModel { ID = "myremoteskeletonID",  Type = ObjectModel.TypeRemoteAssetBundle, Name = "MySkeleton", AssetBundleName = "myskeleton", AssetBundleUri = "https://mraiwebapp.azurewebsites.net/AssetBundles/", positionX = 1f, positionY = -0.9f, positionZ = 3f, rotationX = 0f, rotationY = 0f, rotationZ = 0f, scaleX = 3f, scaleY = 3f, scaleZ = 3f }
+    };
+    public static List<ObjectModel> GetObjectList()
+    {
+        return objectModelItems;
+    }
+} 
+
+
