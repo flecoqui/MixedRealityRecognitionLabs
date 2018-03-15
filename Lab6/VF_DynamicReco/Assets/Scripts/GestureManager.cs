711a9b9c5eb6f2106cd272206a786d36ec74aeb4
Fred Le Coquil <flecoqui@hotmail.com>
2018-03-15 23:01:49 +0100

    Add remote AssetBundle support

---
+++ Lab6/VF_DynamicReco/Assets/Scripts/GestureManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+
+public class GestureManager : MonoBehaviour {
+    public static GestureManager instance;
+    GestureRecognizer recognizer;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Use this for initialization
+    void Start () {
+        recognizer = new GestureRecognizer();
+        recognizer.SetRecognizableGestures(GestureSettings.Tap);
+        recognizer.Tapped += TapHandler;
+        recognizer.StartCapturingGestures();
+    }
+    private void TapHandler(TappedEventArgs obj)
+    {
+        if(Input.GetButton("PannelButton"))
+        {
+            Debug.Log("Button pressed");
+        }
+        else
+        {
+            Debug.Log("Button not pressed");
+        }
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    void OnDestroy()
+    {
+        recognizer.Tapped -= TapHandler;
+
+    }
+}
